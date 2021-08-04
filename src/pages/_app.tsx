f8593f8d39e6287c1888d8f643e7457e9b96194e
Ashley Teo <ashley.teo@ufinity.com>
2021-08-04 15:06:07 +0800

    remove dependencies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,8 @@
 import type { AppProps } from "next/app";
-import { ThemeProvider } from "styled-components";
-import { BaseTheme } from "react-lifesg-design-system";
 
 import "sgds-govtech/css/sgds.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider theme={BaseTheme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+  return <Component {...pageProps} />;
 }
 export default MyApp;
